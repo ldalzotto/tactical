ccf931d0999b4a84304fa47fd0a44ef44cbc7ba9
ldalzotto <loic.dalzotto@hotmail.fr>
2026-08-09 20:06:39 +0200

    More encapsulation of the grid module

--- src/game/grid.h
+++ src/game/grid.h
@@ -4,9 +4,7 @@
 
 #include "../lib/memory.h"
 
-typedef struct {
-    bool walkable;
-} tile_t;
+typedef struct tile tile_t;
 
 SLICE_DEFINE(tile_t);
 
@@ -18,6 +16,5 @@ typedef struct {
 grid_t grid_init(linear_allocator_t *allocator, int width, int height);
 void grid_deinit(linear_allocator_t *allocator, grid_t grid);
 bool grid_in_bounds(grid_t grid, int x, int y);
-tile_t *grid_tile_at(grid_t grid, int x, int y);
 void grid_set_walkable(grid_t grid, int x, int y, bool walkable);
 bool grid_is_walkable(grid_t grid, int x, int y);
