a5f27d8fa0b0b1ed2a27e83c129e5be082a0e339
ldalzotto <loic.dalzotto@hotmail.fr>
2026-08-09 12:49:56 +0200

    Add grid module with walkable tile queries (#31)
    Part of #18

---
+++ src/game/grid.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "../lib/memory.h"
+
+typedef struct {
+    bool walkable;
+} tile_t;
+
+SLICE_DEFINE(tile_t);
+
+typedef struct {
+    int width, height;
+    slice_tile_t tiles;
+} grid_t;
+
+grid_t grid_init(linear_allocator_t *allocator, int width, int height);
+void grid_deinit(linear_allocator_t *allocator, grid_t grid);
+bool grid_in_bounds(grid_t grid, int x, int y);
+tile_t *grid_tile_at(grid_t grid, int x, int y);
+void grid_set_walkable(grid_t grid, int x, int y, bool walkable);
+bool grid_is_walkable(grid_t grid, int x, int y);
